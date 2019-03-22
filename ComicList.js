36d03584a890d24c44a6b5f7aa741c961aaee9e0
Bailly <jeremy.bailly@hotmail.com>
2019-03-22 14:42:51 +0100

    [Add] image + nom + liste des comics + description

---
+++ ComicList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ComicElement from "./ComicElement";
+import { View, Text, FlatList, Dimensions } from "react-native";
+
+import styles from "./style/Comic.style";
+
+class ComicList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.detailComicView}>
+        <FlatList
+          data={this.props.list}
+          renderItem={({ item }) => (
+            <ComicElement
+              key={item.id}
+              resource={item.resourceURI}
+              name={item.title}
+            />
+          )}
+          numColumns={4}
+        />
+      </View>
+    );
+  }
+}
+
+export default ComicList;
--- PageDetails.js
+++ PageDetails.js
@@ -1,41 +1,66 @@
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Avatar } from "react-native-elements";
+import ComicList from "./ComicList";
 
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import styles from "./style/Comic.style";
 
 class PageDetails extends Component {
+  state = {
+    character: null
+  };
 
-    state = {
-            character: null
+  componentDidMount() {
+    //console.log('https://gateway.marvel.com' + '/v1/public/characters/1011334?apikey=' + '28eaf05072bfa7e8bd854d769e3dd9de')
+    fetch(
+      "http://gateway.marvel.com/v1/public/characters/1009144?apikey=673d23d837a3c5a7be3dd08708c919c1",
+      {
+        headers: {
+          Referer: "localhost"
         }
+      }
+    )
+      .then(responseJson => responseJson.json())
+      .then(json => {
+        this.setState({
+          character: json.data.results[0]
+        });
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
 
-    componentDidMount() {
-        console.log('https://gateway.marvel.com' + '/v1/public/characters/1011334?apikey=' + '28eaf05072bfa7e8bd854d769e3dd9de')
-        fetch('http://gateway.marvel.com/v1/public/characters/1011334?apikey=673d23d837a3c5a7be3dd08708c919c1', {
-            headers : {
-                Referer: 'localhost'
-            }
-        }).then(responseJson =>
-           responseJson.json()
-         ).then(json => {
-           this.setState({
-             character: json.data.results[0]
-           })
+  render() {
+    var image =
+      this.state.character &&
+      this.state.character.thumbnail.path +
+        "." +
+        this.state.character.thumbnail.extension;
+    return this.state.character ? (
+      <ScrollView style={styles.detailView}>
+        <View style={styles.detailTitleView}>
+          <Avatar
+            rounded
+            size="xlarge"
+            source={{
+              uri: image
+            }}
+          />
+          <Text style={styles.detailTitleCharacter}>
+            {" "}
+            {this.state.character.name}{" "}
+          </Text>
+          <Text style={styles.detailDescriptionCharacter}>
+            {" "}
+            {this.state.character.description}{" "}
+          </Text>
+        </View>
 
-         }).catch(() => {
-             console.log("error")
-         })
+        <ComicList list={this.state.character.comics.items} />
+      </ScrollView>
+    ) : null;
+  }
+}
 
-     }
-
-    render(){
-        
-        return(
-            <View>
-                <Text> { this.state.character && this.state.character.name } </Text>
-            </View>
-        );
-    }
-
-} 
-
-export default PageDetails
+export default PageDetails;
